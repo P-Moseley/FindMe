499005d93b07880fa6e7fd27739e353a5ddd17ed
P.Moseley <51967142+P-Moseley@users.noreply.github.com>
2020-01-20 18:15:48 +0000

    Delete Drive.java

--- Drive.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-public interface Drive {
-/**
- * @getDrive 	This is a required  methord for all classes impelmenting Drive interface.
- */
-        public String getDrive();
-
-    }
-
